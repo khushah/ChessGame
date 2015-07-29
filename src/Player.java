7a635bd139b3cf6ec7688c2caec86c31c3fdece5
asrayousuf <asrayousuf28@gmail.com>
2015-07-29 15:05:25 +0530

    Added Player Class

---
+++ src/Player.java
@@ -0,0 +1,21 @@
+import java.awt.List;
+import java.util.ArrayList;
+
+
+public class Player {
+	public char color;
+	public ArrayList<ChessPiece> setOfPieces= new ArrayList<ChessPiece>();
+	public char getColor() {
+		return color;
+	}
+	public void setColor(char color) {
+		this.color = color;
+	}
+	public ArrayList<ChessPiece> getSetOfPieces() {
+		return setOfPieces;
+	}
+	public void setSetOfPieces(ArrayList<ChessPiece> setOfPieces) {
+		this.setOfPieces = setOfPieces;
+	}
+	
+}
