b02c74d6a2abddcc2981a9ecf3c1154b47b96565
asrayousuf <asrayousuf28@gmail.com>
2015-07-29 19:06:25 +0530

    Corrected Error in canMove()

--- src/Pawn.java
+++ src/Pawn.java
@@ -1,35 +1,37 @@
+public class Pawn extends ChessPiece {
 
-public class Pawn extends ChessPiece{
-	
 	public static final int STARTW = 2;
 	public static final int STARTB = 7;
 
 	public Pawn(char color, int positionX, int positionY) {
 		super(color, positionX, positionY);
 	}
-	
+
 	@Override
 	public boolean canMoveTo(int destinationX, int destinationY) {
 		return false;
 	}
 
 	public boolean canMoveTo(int destinationX, int destinationY, boolean capture) {
-		if(capture){
-			if(Math.abs(positionX-destinationX)==Math.abs(positionY-destinationY)){
+		if (capture) {
+			if (Math.abs(positionX - destinationX) == Math.abs(positionY
+					- destinationY)) {
 
 				return true;
 			}
-		}else {
-			if((positionY == STARTW && color == 'W') || (positionY == STARTB && color == 'B')){
+		} 
+		else
+		{
+			if ((positionY == STARTW && color == 'W') || (positionY == STARTB && color == 'B')) 
+			{
 				int displacementY = Math.abs(positionY - destinationY);
-				if( ((displacementY == 2) ||
-						(displacementY == 1))&&(positionX == destinationX)){
+				if (((displacementY == 2) || (displacementY == 1)) && (positionX == destinationX)) 
 					return true;
-				}else {
-					if((Math.abs(positionY - destinationY) == 1)&&(positionX == destinationX)){
+			 }
+			else
+			{
+					if ((Math.abs(positionY - destinationY) == 1) && (positionX == destinationX)) 
 						return true;
-					}
-				}
 			}
 			return false;
 		}
