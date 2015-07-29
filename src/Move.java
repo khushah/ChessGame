6b810bbd4d869efcd29fc7da7a96a5a1a979b069
ipsi16 <ipsita16@hotmail.com>
2015-07-29 14:50:03 +0530

    Move class implementation for each function

--- src/Move.java
+++ src/Move.java
@@ -1,7 +1,7 @@
 
 public class Move 
 {
-	int sourceX,sourceY,destinationX,destinationY;
+	int sourceX,sourceY,destinationX,destinationY,displacementX,displacementY;
 	
 	public Move(int sourceX,int sourceY,int destinationX,int destinationY) 
 	{
@@ -9,22 +9,42 @@ public class Move
 		this.sourceY = sourceY;
 		this.destinationX = destinationX;
 		this.destinationY = destinationY;
+		displacementX = Math.abs(sourceX-destinationX);
+		displacementY = Math.abs(sourceY-destinationY);
+		
 	}
 	
 	public boolean isADiagonalMove()
 	{
-		if(Math.abs(sourceX-destinationX)==Math.abs(sourceY-destinationY))
+		if(displacementX==displacementY)
 			return true;
 		return false;
 	}
 	
 	public boolean isAPlusMove()
 	{
-		return false;
+		
+		return isAHorizontalMove()||isAVerticalMove();
 	}
 	
 	public boolean isAnLShapedMove()
 	{
+		if((displacementX==2 && displacementY==1)|| (displacementY==2 && displacementX==1))
+			return true;
+		return false;
+	}
+	
+	public boolean isAHorizontalMove()
+	{
+		if(displacementX!=0 && displacementY==0)
+			return true;
+		return false;
+	}
+	
+	public boolean isAVerticalMove()
+	{
+		if(displacementY!=0 && displacementX==0)
+			return true;
 		return false;
 	}
 
