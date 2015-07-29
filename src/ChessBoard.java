4454fce25966b7477637d82876e3907809f60579
unknown <test@L-MAA-25004240.paypalcorp.com>
2015-07-29 12:39:16 +0530

    Initial design of class ChessBoard

---
+++ src/ChessBoard.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+public class ChessBoard {
+	
+	private ChessPieces chessPieces[][] = new ChessPieces();
+	
+	public void nextMove(String move){
+		return;
+	}
+	
+	public static void main(String[] args){
+		ChessBoard chessBoard = new ChessBoard();
+		chessBoard.nextMove("e4");
+		return;
+	}
+}
