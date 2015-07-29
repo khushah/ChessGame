0b391922d6d8b675675297e6e1d231055b47002c
asrayousuf <asrayousuf28@gmail.com>
2015-07-29 13:47:27 +0530

    Added ChessPiece class

---
+++ src/ChessPiece.java
@@ -0,0 +1,25 @@
+
+public class ChessPiece {
+	
+	public char color;
+	public int positionX;
+	public int positionY;
+	public char getColor() {
+		return color;
+	}
+	public void setColor(char color) {
+		color = color;
+	}
+	public int getPositionX() {
+		return positionX;
+	}
+	public void setPositionX(int positionX) {
+		this.positionX = positionX;
+	}
+	public int getPositionY() {
+		return positionY;
+	}
+	public void setPositionY(int positionY) {
+		this.positionY = positionY;
+	}
+}
